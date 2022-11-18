648050e62da2f2085adb7ba13a51daab264f906d
Camilo María <camilo_jose08@hotmail.com>
2022-11-17 22:52:23 -0400

    Add project files.

---
+++ ParserArithmetic/Aritmetica.cs
@@ -0,0 +1,37 @@
+﻿using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserArithmetic
+{
+    internal class Aritmetica : AritmeticaBaseVisitor<int>
+    {
+        public override int VisitAritmetica([NotNull] AritmeticaParser.AritmeticaContext context)
+        {
+            Console.WriteLine($"Visitando Aritmetica: {context.GetText()}");
+            return base.VisitAritmetica(context);   
+        }
+
+        public override int VisitExpresion([NotNull] AritmeticaParser.ExpresionContext context)
+        {
+            Console.WriteLine($"Visitando Expresion: {context.GetText()}");
+            return base.VisitExpresion(context);
+        }
+
+        public override int VisitFactor([NotNull] AritmeticaParser.FactorContext context)
+        {
+            Console.WriteLine($"Visitando Factor: {context.GetText()}");
+            return base.VisitFactor(context);
+        }
+
+        public override int VisitTermino([NotNull] AritmeticaParser.TerminoContext context)
+        {
+            Console.WriteLine($"Visitando Termino: {context.GetText()}");
+            return base.VisitTermino(context);
+        }
+    }
+}
---
+++ ParserArithmetic/Program.cs
@@ -0,0 +1,14 @@
+﻿using Antlr4.Runtime;
+using ParserArithmetic;
+
+string input = "5*6+9*3-1";
+
+Console.WriteLine($"Input: {input}\n");
+
+ICharStream stream = CharStreams.fromString(input);
+AritmeticaLexer lexer = new AritmeticaLexer(stream);
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+AritmeticaParser parser = new AritmeticaParser(tokens);
+var tree = parser.aritmetica();
+Aritmetica aritmetica = new Aritmetica();
+int x = aritmetica.Visit(tree);
